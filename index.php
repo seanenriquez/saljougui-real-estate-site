91124bf47cb82c7434c4f5e9e875fc0922039f1b
menriquez <mrk.enriquez@gmail.com>
2016-03-29 12:07:32 -0400

    synced up

--- index.php
+++ index.php
@@ -21,25 +21,23 @@ include('includes/header.php');
 <body>
 
 	<div id="container" class="main">
-		<div id="out">                         
-		    
-                
-                    
-                            
-					                <!-- turn top notification bar on here by removing this comment
+		<div id="out">
+			
 
-					                <div class="notify-bar">
-					                <div class="container">
-					                <div class="row">
-					                <div class="col-md-11">
-					                <p>Optional notification bar for displaying important messages to your site vistors. It only shows when you want it to.</p>
-					                </div>
-					                <div class="col-md-1"><a href="#" class="notify-close"><i class="icon-remove-sign"></i></a></div>
-					                </div>
-					                </div>
-					                </div>
+					<!-- turn top notification bar on here by removing this comment
 
-					                -->
+					<div class="notify-bar">
+					<div class="container">
+					<div class="row">
+					<div class="col-md-11">
+					<p>Optional notification bar for displaying important messages to your site vistors. It only shows when you want it to.</p>
+					</div>
+					<div class="col-md-1"><a href="#" class="notify-close"><i class="icon-remove-sign"></i></a></div>
+					</div>
+					</div>
+					</div>
+
+					-->
                              <?php
                             
                             include('includes/nav-bar.php');
